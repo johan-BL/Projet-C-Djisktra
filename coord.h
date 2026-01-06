4c0fc44e0a14b19b0ddc2b642a30a22b2a16ef89
jba3295 <jba3295@casimir.enseeiht.fr>
2026-01-06 16:47:14 +0100

    Ajout coord et grille

---
+++ coord.h
@@ -0,0 +1,27 @@
+#ifndef COORD_H
+#define COORD_H
+#include <stdbool.h>
+#include <stdio.h>
+
+struct coord_t {
+    int abscisse;
+    int ordonnee;
+};
+
+typedef struct coord_t coord_t;
+
+coord_t creer_coord (int abs, int ord);
+
+int get_x (coord_t coord);
+int get_y (coord_t coord);
+
+void set_x (coord_t *coord, int x);
+void set_y (coord_t *coord, int y);
+
+bool memes_coord (coord_t coord1, coord_t coord2);
+
+coord_t translation (coord_t coord, int dx, int dy);
+
+float distance_euclidienne (coord_t coord1, coord_t coord2);
+
+#endif // COORD_H
